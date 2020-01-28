008d79aaffea6421b5678e831c597f1dea7ba989
Alger <AlgerDu@outlook.com>
2020-01-28 12:01:47 +0800

    添加几个接口

---
+++ Src/LogCollect.Abstractions/ICollectContextFields.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D.DevelopTools.LogCollect
+{
+    /// <summary>
+    /// 上下文中的一些自定义字段
+    /// </summary>
+    public interface ICollectContextFields : IDictionary<string, string>
+    {
+    }
+}
---
+++ Src/LogCollect.Abstractions/ICollectFilter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.DevelopTools.LogCollect
+{
+    /// <summary>
+    /// 过滤器
+    /// </summary>
+    public interface ICollectFilter
+    {
+        /// <summary>
+        /// 实列唯一 ID
+        /// </summary>
+        string ID { get; }
+
+        /// <summary>
+        /// 类型码
+        /// </summary>
+        string Code { get; }
+
+        Task Input(ICollectContext context);
+
+        bool SetOutput(Action<ICollectContext> outputAction);
+    }
+}
