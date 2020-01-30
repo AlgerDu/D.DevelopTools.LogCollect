9a2171fe795f9be37130535e4c6bc39e561cac71
Alger <AlgerDu@outlook.com>
2020-01-30 11:08:58 +0800

    添加项目

---
+++ Src/Filters/Filters.Input.File/FileInputFilter.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.DevelopTools.LogCollect.Filters.Input.File
+{
+    public class FileInputFilter : ICollectFilter
+    {
+        ILogger _logger;
+
+        public string ID { get; private set; }
+
+        public string Code => "file-input";
+
+        public FileInputFilter(
+            ILogger<FileInputFilter> logger
+            )
+        {
+            _logger = logger;
+        }
+
+        #region ICollectFilter
+        public bool Init(ICollectFilterOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Input(ICollectContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SetOutput(Action<ICollectContext> outputAction)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
