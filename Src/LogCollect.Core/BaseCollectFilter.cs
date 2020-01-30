c61c5399d70461dcf14e81b1463462787ae1d6df
Alger <AlgerDu@outlook.com>
2020-01-30 19:43:09 +0800

    定义一个 ICollectFilter 基类

---
+++ Src/LogCollect.Core/BaseCollectFilter.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.DevelopTools.LogCollect
+{
+    public abstract class BaseCollectFilter : ICollectFilter
+    {
+        protected ILogger _logger;
+
+        protected Action<ICollectContext> _output;
+
+        #region ICollectFilter
+        public string ID { get; protected set; }
+
+        public abstract string Code { get; }
+        #endregion
+
+        public BaseCollectFilter(
+            ILogger logger
+            )
+        {
+            _logger = logger;
+
+            ID = Guid.NewGuid().ToString();
+        }
+
+        #region ICollectFilter
+        public virtual bool Init(ICollectFilterOptions options)
+        {
+            return true;
+        }
+
+        public virtual bool Run()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual bool Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual Task Input(ICollectContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetOutput(Action<ICollectContext> outputAction)
+        {
+            _output = outputAction;
+        }
+        #endregion
+    }
+}
