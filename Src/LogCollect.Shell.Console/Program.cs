4d7448699196d130b5a9312c5e03f354f39053ff
Alger <AlgerDu@outlook.com>
2020-01-31 14:16:58 +0800

    添加两个项目

---
+++ Src/LogCollect.Shell.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogCollect.Shell.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
