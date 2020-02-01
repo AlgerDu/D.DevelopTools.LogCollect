40a7742bd68c5c5394c370dbbe788dff89eba84f
Alger <AlgerDu@outlook.com>
2020-02-01 13:46:34 +0800

    简单实现读取 json 配置数据

--- Src/LogCollect.Abstractions/ICollectFilterOptions.cs
+++ Src/LogCollect.Abstractions/ICollectFilterOptions.cs
@@ -9,5 +9,16 @@ namespace D.DevelopTools.LogCollect
     /// </summary>
     public interface ICollectFilterOptions
     {
+        /// <summary>
+        /// filter 的类型码
+        /// </summary>
+        string FilterCode { get; }
+
+        /// <summary>
+        /// 获得一个 T 类型的模型；用于 filter 获取各自的对应的配置
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        T ConvertTo<T>();
     }
 }
---
+++ Src/LogCollect.Core/JsonCollectPipelineConfig.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D.DevelopTools.LogCollect
+{
+    /// <summary>
+    /// json 数据类型的配置
+    /// </summary>
+    public class JsonCollectPipelineConfig : ICollectPipelineConfig
+    {
+        JObject _root;
+
+        List<ICollectFilterOptions> _filterOptions;
+
+        public JsonCollectPipelineConfig(
+            string json
+            )
+        {
+            _root = JObject.Parse(json);
+
+            _filterOptions = new List<ICollectFilterOptions>();
+
+            var tmp = _root.First;
+            while (tmp != null)
+            {
+                _filterOptions.Add(new JsonCollectFilterOptions(tmp, ""));
+
+                tmp = tmp.Next;
+            }
+        }
+
+        public IList<ICollectFilterOptions> FilterOptions => _filterOptions;
+    }
+
+    public class JsonCollectFilterOptions : ICollectFilterOptions
+    {
+        JToken _jToken;
+
+        public JsonCollectFilterOptions(
+            JToken jToken
+            , string code
+            )
+        {
+            FilterCode = code;
+            _jToken = jToken;
+        }
+
+        public string FilterCode { get; private set; }
+
+        public T ConvertTo<T>()
+        {
+            return _jToken.ToObject<T>();
+        }
+    }
+}
