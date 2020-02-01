4994c354535074c9ca4c054fe9ed3d4fb7c241e9
Alger <AlgerDu@outlook.com>
2020-02-01 18:13:09 +0800

    添加两个接口

---
+++ Src/LogCollect.Abstractions/ICollectFilterCollector.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D.DevelopTools.LogCollect
+{
+    public interface ICollectFilterProvider
+    {
+        IDictionary<string, Type> Get();
+    }
+}
