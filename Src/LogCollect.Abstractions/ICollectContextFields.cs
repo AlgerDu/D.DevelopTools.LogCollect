ef26f8b42c652edc94de7e66c0f8db24125f666c
Alger <AlgerDu@outlook.com>
2020-02-02 12:20:02 +0800

    Update ICollectContextFields.cs

--- Src/LogCollect.Abstractions/ICollectContextFields.cs
+++ Src/LogCollect.Abstractions/ICollectContextFields.cs
@@ -7,7 +7,10 @@ namespace D.DevelopTools.LogCollect
     /// <summary>
     /// 上下文中的一些自定义字段
     /// </summary>
-    public interface ICollectContextFields : IDictionary<string, string>
+    public interface ICollectContextFields
     {
+        string this[string key] { get; set; }
+
+        bool Remove(string key);
     }
 }
