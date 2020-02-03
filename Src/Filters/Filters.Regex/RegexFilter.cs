ddc777bf3035cd6f6f1541b459099ca6a2ee9993
Alger <AlgerDu@outlook.com>
2020-02-03 11:26:43 +0800

    添加 RegexFilter 项目

---
+++ Src/Filters/Filters.Regex/RegexFilter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D.DevelopTools.LogCollect.Filters.DRegex
+{
+    internal class RegexFilter : BaseCollectFilter, ICollectFilter
+    {
+        public const string CCode = "regex";
+
+        public override string Code => CCode;
+
+        public RegexFilter(
+            ILogger<RegexFilter> logger
+            ) : base(logger)
+        {
+
+        }
+    }
+}
