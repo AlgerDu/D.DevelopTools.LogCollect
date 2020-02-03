0b989cf4f74eba8b782f69f0694c88d8a19db52f
Alger <AlgerDu@outlook.com>
2020-02-03 13:35:05 +0800

    可用的正则表达式过滤器

--- Src/Filters/Filters.Regex/RegexFilter.cs
+++ Src/Filters/Filters.Regex/RegexFilter.cs
@@ -1,13 +1,18 @@
 ﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace D.DevelopTools.LogCollect.Filters.DRegex
 {
     internal class RegexFilter : BaseCollectFilter, ICollectFilter
     {
         public const string CCode = "regex";
+
+        List<FieldOptions> _fieldOptions;
 
         public override string Code => CCode;
 
@@ -16,6 +21,57 @@ namespace D.DevelopTools.LogCollect.Filters.DRegex
             ) : base(logger)
         {
 
+        }
+
+        public override bool Init(ICollectFilterOptions options)
+        {
+            _fieldOptions = new List<FieldOptions>();
+
+            var json = options.ToString();
+
+            var root = JObject.Parse(json);
+
+            var p = root.First;
+            while (p != null)
+            {
+                foreach (JProperty c in root[p.Path].Children())
+                {
+                    var fo = new FieldOptions();
+                    fo.SrcField = p.Path;
+                    fo.DstField = c.Name;
+                    fo.Pattern = c.Value.ToString();
+
+                    _fieldOptions.Add(fo);
+                }
+
+                p = p.Next;
+            }
+
+            return true;
+        }
+
+        public override Task Input(ICollectContext context)
+        {
+            return Task.Run(() =>
+            {
+                foreach (var o in _fieldOptions)
+                {
+                    var src = context.Fields[o.SrcField];
+
+                    if (string.IsNullOrEmpty(src))
+                    {
+                        context.Fields[o.DstField] = "";
+                    }
+                    else
+                    {
+                        var gs = Regex.Match(src, o.Pattern).Groups;
+
+                        context.Fields[o.DstField] = gs[gs.Count - 1].Value;
+                    }
+                }
+
+                _output(context);
+            });
         }
     }
 }
