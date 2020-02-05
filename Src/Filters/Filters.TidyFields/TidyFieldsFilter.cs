71c0f2e7e078311952e3c97b4a437ddb485271b8
Alger <AlgerDu@outlook.com>
2020-02-05 21:44:11 +0800

    TidyFieldsFilter

---
+++ Src/Filters/Filters.TidyFields/TidyFieldsFilter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D.DevelopTools.LogCollect.Filters.TidyFields
+{
+    public class TidyFieldsFilter : BaseCollectFilter
+    {
+        public const string CCode = "tidy-fields";
+
+        public override string Code => CCode;
+
+        public TidyFieldsFilter(
+            ILogger<TidyFieldsFilter> logger
+            ) : base(logger)
+        {
+
+        }
+    }
+}
