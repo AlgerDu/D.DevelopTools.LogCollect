081c9f581c61ccb13c69a5cb189737348d503570
Alger <AlgerDu@outlook.com>
2020-02-06 11:07:15 +0800

    ElasticsearchOutputFilter

---
+++ Src/Filters/Filter.Output.Elasticsearch/ElasticsearchOutputFilter.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.DevelopTools.LogCollect.Filters.Output.Elasticsearch
+{
+    internal class ElasticsearchOutputFilter : BaseCollectFilter
+    {
+        public const string CCode = "elasticsearch-output";
+
+        ElasticsearchOutputFilterOptions _options;
+        HttpClient _http;
+
+        public override string Code => CCode;
+
+        public ElasticsearchOutputFilter(
+            ILogger<ElasticsearchOutputFilter> logger
+            ) : base(logger)
+        {
+            _http = new HttpClient();
+        }
+
+        public override bool Init(ICollectFilterOptions options)
+        {
+            _options = options.ConvertTo<ElasticsearchOutputFilterOptions>();
+
+            return true;
+        }
+
+        public override Task Input(ICollectContext context)
+        {
+            return Task.Run(() =>
+            {
+                var body = new JObject();
+
+                foreach (var f in _options.Fields)
+                {
+                    body[f] = context.Fields[f];
+                }
+
+                var index = context.Fields[_options.Index];
+                var type = context.Fields[_options.Type];
+
+                var content = new StringContent(body.ToString());
+                content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+
+                try
+                {
+                    _http.PostAsync($"http://{_options.Host}/{index}/{type}", content);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogWarning($"{ex}");
+                }
+            });
+        }
+    }
+}
---
+++ Src/Filters/Filter.Output.Elasticsearch/ElasticsearchOutputFilterProvider.cs
@@ -0,0 +1,18 @@
+﻿using D.DevelopTools.LogCollect.Filters.Output.Elasticsearch;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D.DevelopTools.LogCollect.Filters
+{
+    public class ElasticsearchOutputFilterProvider : ICollectFilterProvider
+    {
+        public IDictionary<string, Type> Get()
+        {
+            return new Dictionary<string, Type>
+            {
+                {ElasticsearchOutputFilter.CCode,typeof(ElasticsearchOutputFilter) }
+            };
+        }
+    }
+}
