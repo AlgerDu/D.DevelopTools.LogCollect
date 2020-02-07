423477d357f7cd01494bd3a70dd2adb4fea3c673
Alger <AlgerDu@outlook.com>
2020-02-07 23:45:53 +0800

    Create Enums.cs

---
+++ Src/LogCollect.Core/Enums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D.DevelopTools.LogCollect
+{
+    public enum FilterState
+    {
+        Stop,
+        Running,
+        Pause
+    }
+}
