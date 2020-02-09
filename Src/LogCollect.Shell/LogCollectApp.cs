08c847dd8d45715193eac53b0d96550e8cc53881
Alger <AlgerDu@outlook.com>
2020-02-09 22:10:44 +0800

    Update LogCollectApp.cs

--- Src/LogCollect.Shell/LogCollectApp.cs
+++ Src/LogCollect.Shell/LogCollectApp.cs
@@ -12,18 +12,18 @@ namespace D.DevelopTools.LogCollect
         ILogger _logger;
         IHostingEnvironment _environment;
 
-        ICollectFilterFactory _filterFactory;
+        ICollectPipelineFactory _pipelineFactory;
 
         public LogCollectApp(
             ILogger<LogCollectApp> logger
             , IHostingEnvironment environment
-            , ICollectFilterFactory filterFactory
+            , ICollectPipelineFactory pipelineFactory
             )
         {
             _logger = logger;
             _environment = environment;
 
-            _filterFactory = filterFactory;
+            _pipelineFactory = pipelineFactory;
         }
 
         public IApplication Run()
@@ -31,41 +31,11 @@ namespace D.DevelopTools.LogCollect
             _logger.LogInformation($"{this} start to run");
 
             var json = File.ReadAllText($"{_environment.ContentRootPath}Configs/log-collect-example-config.json");
-
             var config = new JsonCollectPipelineConfig(json);
 
-            var fileInput = _filterFactory.Create(config.FilterOptions[0].FilterCode);
-            var regex = _filterFactory.Create(config.FilterOptions[1].FilterCode);
-            var tidy = _filterFactory.Create(config.FilterOptions[2].FilterCode);
-            var es = _filterFactory.Create(config.FilterOptions[3].FilterCode);
+            var pipeline = _pipelineFactory.Create(config);
 
-            fileInput.SetOutput((context) =>
-            {
-                return regex.Input(context);
-            });
-
-            regex.SetOutput((context) =>
-            {
-                return tidy.Input(context);
-            });
-
-            tidy.SetOutput((context) =>
-            {
-                return es.Input(context);
-            });
-
-            es.SetEmpty((filter) =>
-            {
-                fileInput.Run();
-            });
-
-            fileInput.Init(config.FilterOptions[0]);
-            regex.Init(config.FilterOptions[1]);
-            tidy.Init(config.FilterOptions[2]);
-            es.Init(config.FilterOptions[3]);
-
-            es.Run();
-            fileInput.Run();
+            pipeline.Run();
 
             _logger.LogInformation($"{this} is running");
             return this;
