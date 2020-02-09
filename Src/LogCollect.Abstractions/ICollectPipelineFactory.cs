2ffa72e0cf6ab160f6b95a88a7ef4b729a8d21ab
Alger <AlgerDu@outlook.com>
2020-02-09 14:32:39 +0800

    Create ICollectPipelineFactory.cs

---
+++ Src/LogCollect.Abstractions/ICollectPipelineFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D.DevelopTools.LogCollect
+{
+    /// <summary>
+    /// 管道工厂
+    /// </summary>
+    public interface ICollectPipelineFactory
+    {
+        /// <summary>
+        /// 新建一个管道
+        /// </summary>
+        /// <param name="config"></param>
+        /// <returns></returns>
+        ICollectPipeline Create(ICollectPipelineConfig config);
+    }
+}
